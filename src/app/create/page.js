6fc6be3f48d8b36d7bdda30f28a9c2d115c64cae
박윤환 <park-yunhwan@bag-yunhwan-ui-MacBookPro.local>
2023-08-04 14:32:09 +0900

    feat: 글 생성 기능 구현

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,7 +1,42 @@
-export default function Create(){
+'use client'
 
+
+//next router가 아닌 navigation에서 가져오기
+import { useRouter } from "next/navigation"
+
+export default function Create(){
+    const router = useRouter()
     return(
-        <>Create</>
+        <form onSubmit={(e)=>{
+            e.preventDefault()
+            const title =e.target.title.value
+            const body = e.target.body.value
+            const options={
+                method: 'POST',
+                headers :{
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({title,body})
+            }
+            fetch('http://localhost:9999/topics',options)
+                .then((res)=>res.json())
+                .then((res)=>{
+                    console.log(res)
+                    const lastId = res.id;
+                    router.push(`/read/${lastId}`)
+                })
+
+       }}>
+            <p>
+                <input type='text' name='title' placeholder="title" />
+            </p>
+            <p>
+                <textarea name='body' placeholder="body" />
+            </p>
+            <p>
+              <input type='submit' value='create'/>
+            </p>
+        </form>
     )
 
 
